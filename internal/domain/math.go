21daa12d41e952d263473476af2e9ee9e4a6a1a1
MokkeMeguru <meguru.mokke@gmail.com>
2021-12-17 17:12:33 +0900

    wip3

--- internal/domain/math.go
+++ internal/domain/math.go
@@ -1,9 +1,22 @@
 package domain
 
+import "golang.org/x/xerrors"
+
 func Plus(a, b int) int {
 	return a + b
 }
 
 func Minus(a, b int) int {
 	return a - b
+}
+
+func Multiple(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, xerrors.New("invalid value 0")
+	}
+	return a / b, nil
 }
